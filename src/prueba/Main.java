e51473aebfffdef7179748bfb7985c32a5733d2f
Reniery-LB <relu_23@alu.uabcs.mx>
2025-02-21 19:43:51 -0700

    prueba de push

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -2,9 +2,15 @@ package prueba;
 
 public class Main {
 
+	public Main() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola");
+		
+		System.out.println("Adios");
 	}
 
 }
